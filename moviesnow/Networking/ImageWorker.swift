a535d23660aea43bbe6cd7127af75e3eac5979e6
DiegoAlfaroV <d_alfarovives@hotmail.com>
2019-04-15 00:54:21 -0500

    Update pods. TODO: Image API

---
+++ moviesnow/Networking/ImageWorker.swift
@@ -0,0 +1,13 @@
+//
+//  ImageWorker.swift
+//  moviesnow
+//
+//  Created by Diego on 14/04/19.
+//  Copyright © 2019 IQVIA. All rights reserved.
+//
+
+import UIKit
+
+class ImageWorker: NSObject {
+
+}
